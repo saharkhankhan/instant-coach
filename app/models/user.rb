0336658c1202c1302c7f76338340923f286509c6
sahar khan <saharkhan1911@gmail.com>
2019-02-25 18:45:11 +0000

    devises has been added so now have a sign up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   has_many :bookings
   has_many :sessions
   has_many :athlete_reviews, class_name: :Review, foreign_key: "athlete_id"
