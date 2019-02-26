51eda6fc6766618825304d747fad6da097066506
Esther Chan <est.chanwh@gmail.com>
2019-02-26 11:45:02 +0000

    added two controllers - users and sessions. Not done yet - will be back after modifying the schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
+  members do
+    resources :sessions
+  end
   root to: 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
