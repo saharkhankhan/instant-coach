085f995fa67394273f49fa6c9f58aa04bd10b463
sahar khan <saharkhan1911@gmail.com>
2019-02-26 12:51:31 +0000

    started seeding athletes and coaches but need to update dependent-destroy in models

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_02_26_123714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "athelete_sports", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_athelete_sports_on_user_id"
+  end
 
   create_table "bookings", force: :cascade do |t|
     t.date "date"
@@ -25,6 +32,14 @@ ActiveRecord::Schema.define(version: 2019_02_26_123714) do
     t.datetime "updated_at", null: false
     t.index ["session_id"], name: "index_bookings_on_session_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "coached_sports", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_coached_sports_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -77,8 +92,10 @@ ActiveRecord::Schema.define(version: 2019_02_26_123714) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "athelete_sports", "users"
   add_foreign_key "bookings", "sessions"
   add_foreign_key "bookings", "users"
+  add_foreign_key "coached_sports", "users"
   add_foreign_key "sessions", "sports"
   add_foreign_key "sessions", "users"
 end
