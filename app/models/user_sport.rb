06208d82edbc6f425ecdb3be6ee9749a561cdd3b
sahar khan <saharkhan1911@gmail.com>
2019-02-26 18:04:15 +0000

    removed sports controller and added user-sports-controller

--- app/models/user_sport.rb
+++ app/models/user_sport.rb
@@ -1,4 +1,4 @@
 class UserSport < ApplicationRecord
   belongs_to :user, class_name: :User, foreign_key: "user_id"
-  belongs_to :sport
+  belongs_to :sport, class_name: :Sport, foreign_key: "sport_id"
 end
