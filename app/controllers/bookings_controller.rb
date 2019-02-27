b6fb2832092d1edb583c9c8c0c8106d5e8b74acf
Alex Elsden <alex.elsden@hotmail.co.uk>
2019-02-27 12:24:25 +0000

    changed bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,29 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @booking = Booking.new(booking_params)
+    @booking.athlete = @user
+    if @booking.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @user = @booking.coach
+    @booking.destroy
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :time, :sessions_id)
+  end
 end
