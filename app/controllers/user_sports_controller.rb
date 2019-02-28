f1260d100ba4fc5fd948a68aa46788fa938bbdca
sahar khan <saharkhan1911@gmail.com>
2019-02-28 08:56:34 +0000

    finished the form to add user sports

--- app/controllers/user_sports_controller.rb
+++ app/controllers/user_sports_controller.rb
@@ -6,4 +6,27 @@ class UserSportsController < ApplicationController
   def show
     @user_sport = UserSport.find(params[:sport_id])
   end
+
+  def new
+    @user = current_user
+    @user_sport = UserSport.new
+  end
+
+  def create
+    @user_sport = UserSport.new()
+    @user_sport.user = current_user
+    sport = Sport.find(params[:user_sport][:sport_id].to_i)
+    @user_sport.sport = sport
+    if @user_sport.save
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:user_sport).permit(:sport, :user_sport_id, :user)
+  end
 end
