57707d2826763ba48daa1bcef44cde17f7c2621c
sahar khan <saharkhan1911@gmail.com>
2019-03-01 11:07:40 +0000

    Fixed the issue I caused to the map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,9 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 
 import { initMapbox } from '../plugins/init_mapbox';
 
+initMapbox();
+
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
 
-initMapbox();
 
