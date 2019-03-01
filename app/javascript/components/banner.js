de0df267d8fc1683c4198c9177dd7ead240fb96f
sahar khan <saharkhan1911@gmail.com>
2019-03-01 10:43:03 +0000

    typewritter added to homepage

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["Achieve Your Goals", "Find a Coach Near You Today"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
