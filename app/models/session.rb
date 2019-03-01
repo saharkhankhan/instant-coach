3302c0908d19bad52c0d79ce55a3d64b4e061034
sahar khan <saharkhan1911@gmail.com>
2019-03-01 12:02:27 +0000

    redirects coaches to their dashboard on sign up:

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,6 @@
 class Session < ApplicationRecord
   belongs_to :coach, class_name: :User, foreign_key: "coach_id"
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
 end
